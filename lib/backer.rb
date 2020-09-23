5f3806badfab016a74f3386b447d8b67984687d3
Travis Lee <travisslee223@gmail.com>
2020-09-23 20:10:10 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  
+  attr_accessor :name 
+  
+  attr_reader :backed_projects
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    @@all << self
+  end
+  
+  def back_project(project_name)
+    @backed_project << Project.new(project_name)
+  end
+end
